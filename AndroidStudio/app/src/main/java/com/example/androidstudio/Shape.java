c16c5d33794a409e63ccbc838efbc077d8e0a580
King Fu <42598596+kingfxj@users.noreply.github.com>
2019-10-02 18:42:40 -0600

    Update Shape.java

--- AndroidStudio/app/src/main/java/com/example/androidstudio/Shape.java
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/Shape.java
@@ -3,6 +3,7 @@ package com.example.androidstudio;
 public abstract class Shape {
     private int x;
     private int y;
+    private String color = "blue";
 
     public void setX(int x) {
         this.x = x;
