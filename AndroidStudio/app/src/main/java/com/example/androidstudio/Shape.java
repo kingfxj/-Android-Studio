5ddaa4b8f7440eaac1c0c1cb2c31c286f983f097
Shelly <shellyxue99@icloud.com>
2019-10-02 18:42:47 -0600

    Update the color

--- AndroidStudio/app/src/main/java/com/example/androidstudio/Shape.java
+++ AndroidStudio/app/src/main/java/com/example/androidstudio/Shape.java
@@ -3,7 +3,7 @@ package com.example.androidstudio;
 public abstract class Shape {
     private int x;
     private int y;
-    String colour = "blue";
+    private String colour = "blue";
 
     public void setX(int x) {
         this.x = x;
